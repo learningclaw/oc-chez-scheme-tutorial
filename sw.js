b268ad06bf72f92ce27b554e2cb9afc9946a49f0
Markus <markus@Mac.lan>
2026-03-01 16:11:58 -0800

    fix: constrain page width, prevent content overflow on mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chez-scheme-v3';
+const CACHE = 'chez-scheme-v4';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
