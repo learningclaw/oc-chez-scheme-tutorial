e2c9b10d91c3dad0ec3cd908dddb51f4a1f29c3c
Markus <markus@Mac.lan>
2026-03-01 16:29:08 -0800

    fix: separate desktop/mobile width logic, mobile uses full 100vw

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chez-scheme-v6';
+const CACHE = 'chez-scheme-v7';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
