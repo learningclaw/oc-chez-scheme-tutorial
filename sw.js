03cfc7efad8838b7c9d5384a597588cd227d0c27
Markus <markus@MarkusMac.local>
2026-03-01 23:33:25 -0800

    fix: keep H3 subsections together with parent H2, increase page content threshold

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chez-scheme-v11';
+const CACHE = 'chez-scheme-v12';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
