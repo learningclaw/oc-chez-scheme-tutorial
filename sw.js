950d0cec0557e8820d8468668cc9bc7b3b9fb476
Markus <markus@Mac.lan>
2026-03-01 16:31:28 -0800

    fix: replace all 100vw with 100%, use inset:0 for pages, zero padding on main

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chez-scheme-v7';
+const CACHE = 'chez-scheme-v8';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
