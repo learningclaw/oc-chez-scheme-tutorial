9db83936221a5b4332d6b93389768a3df131fe63
Markus <markus@Mac.lan>
2026-03-01 16:24:25 -0800

    rewrite: stacked page layout, fix width overflow and swipe

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chez-scheme-v4';
+const CACHE = 'chez-scheme-v5';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
